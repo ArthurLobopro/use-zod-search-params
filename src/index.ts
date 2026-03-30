88abda5c01bf2de6f521dc3f1b9b25660f379132
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2026-03-30 12:02:59 -0300

    refactor: use factory design  pattern to build hook callbacks

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,11 @@
 import { useEffect, useMemo } from "react";
-import { useSearchParams } from "react-router-dom";
+import { type SetURLSearchParams, useSearchParams } from "react-router-dom";
 import type { ZodDefault, z } from "zod";
-
-type BaseAllowedTypes =
-  | z.ZodString
-  | z.ZodCoercedNumber
-  | z.ZodCoercedBoolean
-  | z.ZodEnum;
-
-type MiddleTypes =
-  | z.ZodDefault<BaseAllowedTypes>
-  | z.ZodNullable<BaseAllowedTypes>;
-
-// Allow to Agregate .nullable().default()
-type AllowedTypes =
-  | MiddleTypes
-  | z.ZodDefault<MiddleTypes>
-  | z.ZodNullable<MiddleTypes>;
-
-type SnakeToCamel<S extends string> = S extends `${infer Head}_${infer Tail}`
-  ? `${Head}${Capitalize<SnakeToCamel<Tail>>}`
-  : S;
-type SetterName<T extends string> = `set${Capitalize<SnakeToCamel<T>>}`;
-
-const setterName = <T extends string>(str: T) => {
-  return [
-    "set",
-    str[0]!.toUpperCase(),
-    str.slice(1).replace(/_([a-z0-9])/g, (g) => g[1]!.toUpperCase()),
-  ].join("") as SetterName<T>;
-};
-
-interface UseZodSearchOptions {
-  onParseError: "throw" | "clean";
-}
+import type { AllowedTypes, SetterName, UseZodSearchOptions } from "./types";
 
 const DEFAULT_OPTIONS = {
   onParseError: "clean",
+  clearDefaults: false,
 } satisfies UseZodSearchOptions;
 
 export function useZodSearchParams<
@@ -46,7 +15,67 @@ export function useZodSearchParams<
 >(schema: S, options: UseZodSearchOptions = DEFAULT_OPTIONS) {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
+  useEffect(
+    UpdateParamsFactory(schema, options, searchParams, setSearchParams),
+    [searchParams],
+  );
+
+  const params = useMemo(
+    ParamsGetterObjectFunctionFactory(schema, options, searchParams),
+    [searchParams, schema],
+  );
+
+  function setPartial(data: Partial<z.infer<S>>) {
+    const params = new URLSearchParams(searchParams);
+
+    for (const key in data) {
+      //@ts-expect-error Trust me TypeScript
+      const parsedValue = schema.shape[key].parse(data[key]);
+
+      if (parsedValue) {
+        params.set(key, String(parsedValue));
+      } else {
+        params.delete(key);
+      }
+    }
+
+    setSearchParams(params);
+  }
+
+  function getSetter<K extends keyof S["shape"]>(key: K) {
+    return (v: z.infer<S["shape"][K]>) =>
+      setPartial({ [key]: v } as Partial<z.infer<S>>);
+  }
+
+  const setters = {} as {
+    [Key in K as SetterName<string & Key>]: (
+      value: z.infer<S["shape"][Key]>,
+    ) => void;
+  };
+
+  for (const key of Object.keys(schema.shape) as K[]) {
+    //@ts-expect-error Trust me TypeScript
+    setters[SetterNameFactory(key)] = getSetter(key);
+  }
+
+  return {
+    params,
+    setters,
+    setParams: setPartial,
+  };
+}
+
+function UpdateParamsFactory<
+  T extends Record<string, AllowedTypes>,
+  S extends z.ZodObject<T>,
+  K extends keyof S["shape"],
+>(
+  schema: S,
+  options: UseZodSearchOptions,
+  searchParams: URLSearchParams,
+  setSearchParams: SetURLSearchParams,
+) {
+  return () => {
     let hasChanged = false;
 
     for (const key of Object.keys(schema.shape) as K[]) {
@@ -65,46 +94,22 @@ export function useZodSearchParams<
     if (hasChanged) {
       setSearchParams(searchParams);
     }
-  }, [searchParams]);
-
-  function setPartial(data: Partial<z.infer<S>>) {
-    const params = new URLSearchParams(searchParams);
-
-    for (const key in data) {
-      //@ts-expect-error Trust me TypeScript
-      const parsedValue = schema.shape[key].parse(data[key]);
-
-      if (parsedValue) {
-        params.set(key, String(parsedValue));
-      } else {
-        params.delete(key);
-      }
-    }
-    setSearchParams(params);
-  }
-
-  function getSetter<K extends keyof S["shape"]>(key: K) {
-    return (v: z.infer<S["shape"][K]>) =>
-      setPartial({ [key]: v } as Partial<z.infer<S>>);
-  }
-
-  const setters = {} as {
-    [Key in K as SetterName<string & Key>]: (
-      value: z.infer<S["shape"][Key]>,
-    ) => void;
   };
+}
 
-  for (const key of Object.keys(schema.shape) as K[]) {
-    //@ts-expect-error Trust me TypeScript
-    setters[setterName(key)] = getSetter(key);
-  }
-
-  const params = useMemo(() => {
+function ParamsGetterObjectFunctionFactory<
+  T extends Record<string, AllowedTypes>,
+  S extends z.ZodObject<T>,
+  K extends keyof S["shape"],
+>(schema: S, options: UseZodSearchOptions, searchParams: URLSearchParams) {
+  return () => {
     const obj = {} as z.infer<S>;
 
     for (const key of Object.keys(schema.shape) as K[]) {
       Object.defineProperty(obj, key, {
         get() {
+          const valueSchema = schema.shape[key];
+
           try {
             const hasValue = searchParams.has(key as string);
             const searchValue = searchParams.get(key as string);
@@ -113,13 +118,13 @@ export function useZodSearchParams<
               return schema.shape[key]?.parse(searchValue);
             }
 
-            return (schema.shape[key] as ZodDefault)?.def?.defaultValue ?? null;
+            return isDefault(valueSchema) ? valueSchema.def.defaultValue : null;
           } catch (error) {
             if (options.onParseError === "throw") {
               throw error;
             }
 
-            return (schema.shape[key] as ZodDefault)?.def?.defaultValue ?? null;
+            return isDefault(valueSchema) ? valueSchema.def.defaultValue : null;
           }
         },
         enumerable: true,
@@ -127,11 +132,18 @@ export function useZodSearchParams<
     }
 
     return obj;
-  }, [searchParams, schema]);
-
-  return {
-    params,
-    setters,
-    setParams: setPartial,
   };
+}
+
+const SetterNameFactory = <T extends string>(str: T) => {
+  return [
+    "set",
+    str[0]!.toUpperCase(),
+    str.slice(1).replace(/_([a-z0-9])/g, (g) => g[1]!.toUpperCase()),
+  ].join("") as SetterName<T>;
+};
+
+// biome-ignore lint/suspicious/noExplicitAny: v must be any because it's a type guard function
+function isDefault(v: any): v is ZodDefault {
+  return (v as ZodDefault)?.def?.defaultValue !== undefined;
 }
---
+++ src/types.ts
@@ -0,0 +1,29 @@
+import type z from "zod";
+
+export type BaseAllowedTypes =
+  | z.ZodString
+  | z.ZodCoercedNumber
+  | z.ZodCoercedBoolean
+  | z.ZodEnum;
+
+export type MiddleTypes =
+  | z.ZodDefault<BaseAllowedTypes>
+  | z.ZodNullable<BaseAllowedTypes>;
+
+// Allow to Agregate .nullable().default()
+export type AllowedTypes =
+  | MiddleTypes
+  | z.ZodDefault<MiddleTypes>
+  | z.ZodNullable<MiddleTypes>;
+
+export type SnakeToCamel<S extends string> =
+  S extends `${infer Head}_${infer Tail}`
+    ? `${Head}${Capitalize<SnakeToCamel<Tail>>}`
+    : S;
+
+export type SetterName<T extends string> = `set${Capitalize<SnakeToCamel<T>>}`;
+
+export interface UseZodSearchOptions {
+  onParseError: "throw" | "clean";
+  clearDefaults?: boolean;
+}
